1afa3a241daf0382cc51a1d66b1930bce098f06d
Kamil <1505kamil@gmail.com>
2025-04-08 01:43:36 +0500

    debug elements removed

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,18 +2,11 @@ package ru.kata.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
-		PasswordEncoder encoder = new BCryptPasswordEncoder();
-		String raw = "admin";
-		String hash = encoder.encode(raw);
-		System.out.println("Хэш: " + hash);
-		System.out.println("Сравнение: " + encoder.matches("admin", hash));
 	}
 }
